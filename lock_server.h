b061bd79d389f4afa673d99a1880e125b289a25e
Tong Ruizhe <tongruizhe@sjtu.edu.cn>
2018-10-09 19:23:22 +0800

    lab2 part2a finish

--- lock_server.h
+++ lock_server.h
@@ -5,21 +5,30 @@
 #define lock_server_h
 
 #include <string>
+#include <map>
 #include "lock_protocol.h"
 #include "lock_client.h"
 #include "rpc.h"
+#include <pthread.h>
 
 class lock_server {
 
- protected:
-  int nacquire;
+protected:
+    int nacquire;
+    pthread_mutex_t lock;
+    std::map<lock_protocol::lockid_t, pthread_cond_t *> cond_vars;
+    std::map<lock_protocol::lockid_t, uint32_t> lock_state;
 
- public:
-  lock_server();
-  ~lock_server() {};
-  lock_protocol::status stat(int clt, lock_protocol::lockid_t lid, int &);
-  lock_protocol::status acquire(int clt, lock_protocol::lockid_t lid, int &);
-  lock_protocol::status release(int clt, lock_protocol::lockid_t lid, int &);
+public:
+    lock_server();
+
+    ~lock_server() {};
+
+    lock_protocol::status stat(int clt, lock_protocol::lockid_t lid, int &);
+
+    lock_protocol::status acquire(int clt, lock_protocol::lockid_t lid, int &);
+
+    lock_protocol::status release(int clt, lock_protocol::lockid_t lid, int &);
 };
 
 #endif 
